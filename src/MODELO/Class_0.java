9a2f8517c1df3835fe884f46ec52da46670fe9c3
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2025-03-06 12:11:49 -0500

    Errores en en El Formulario ejercicio1

--- src/MODELO/Class_0.java
+++ src/MODELO/Class_0.java
@@ -1,13 +1,17 @@
+package MODELO;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package MODELO;
 
 /**
  *
  * @author Propietario
  */
-public class NewClass {
-    
+public class Class_0 {
+    public int multiplicar;
+    public void metodoTriangulo( int nu1,int nu2){
+        multiplicar = nu1*nu2;
+    }
 }
---
+++ src/MODELO/Class_01.java
@@ -0,0 +1,15 @@
+
+package MODELO;
+
+
+public class Class_01 {
+    
+  public int suma, resta, multi;
+  
+  public void metodo_suma(int n1, int n2)
+  {
+  
+  suma=n1+n2;
+  }
+    
+}
